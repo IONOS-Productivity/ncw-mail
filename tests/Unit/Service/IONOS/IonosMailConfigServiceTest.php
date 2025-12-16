67e6cd77cfc8134a639bd9a6c35ca772f587bdb9
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-12-16 15:08:05 +0100

    IONOS(ionos-mail): refactor(service): enhance IonosMailConfigService availability check
    Update isMailConfigAvailable to check for local account configuration:
    - Add AccountService and IUserSession dependencies
    - Check if user has remote IONOS account
    - Check if remote account is configured locally in mail app
    - Show configuration if remote exists but local doesn't (retry scenario)
    This allows users to complete account setup if the initial Nextcloud
    account creation failed but the IONOS account was created.
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- tests/Unit/Service/IONOS/IonosMailConfigServiceTest.php
+++ tests/Unit/Service/IONOS/IonosMailConfigServiceTest.php
@@ -10,15 +10,20 @@ declare(strict_types=1);
 namespace OCA\Mail\Tests\Unit\Service\IONOS;
 
 use ChristophWurst\Nextcloud\Testing\TestCase;
+use OCA\Mail\Service\AccountService;
 use OCA\Mail\Service\IONOS\IonosConfigService;
 use OCA\Mail\Service\IONOS\IonosMailConfigService;
 use OCA\Mail\Service\IONOS\IonosMailService;
+use OCP\IUser;
+use OCP\IUserSession;
 use PHPUnit\Framework\MockObject\MockObject;
 use Psr\Log\LoggerInterface;
 
 class IonosMailConfigServiceTest extends TestCase {
 	private IonosConfigService&MockObject $ionosConfigService;
 	private IonosMailService&MockObject $ionosMailService;
+	private AccountService&MockObject $accountService;
+	private IUserSession&MockObject $userSession;
 	private LoggerInterface&MockObject $logger;
 	private IonosMailConfigService $service;
 
@@ -27,11 +32,15 @@ class IonosMailConfigServiceTest extends TestCase {
 
 		$this->ionosConfigService = $this->createMock(IonosConfigService::class);
 		$this->ionosMailService = $this->createMock(IonosMailService::class);
+		$this->accountService = $this->createMock(AccountService::class);
+		$this->userSession = $this->createMock(IUserSession::class);
 		$this->logger = $this->createMock(LoggerInterface::class);
 
 		$this->service = new IonosMailConfigService(
 			$this->ionosConfigService,
 			$this->ionosMailService,
+			$this->accountService,
+			$this->userSession,
 			$this->logger,
 		);
 	}
@@ -41,43 +50,159 @@ class IonosMailConfigServiceTest extends TestCase {
 			->method('isIonosIntegrationEnabled')
 			->willReturn(false);
 
-		$this->ionosMailService->expects($this->never())
-			->method('mailAccountExistsForCurrentUser');
+		$this->userSession->expects($this->never())
+			->method('getUser');
 
 		$result = $this->service->isMailConfigAvailable();
 
 		$this->assertFalse($result);
 	}
 
-	public function testIsMailConfigAvailableReturnsTrueWhenUserHasNoAccount(): void {
+	public function testIsMailConfigAvailableReturnsFalseWhenNoUserSession(): void {
 		$this->ionosConfigService->expects($this->once())
 			->method('isIonosIntegrationEnabled')
 			->willReturn(true);
+
+		$this->userSession->expects($this->once())
+			->method('getUser')
+			->willReturn(null);
+
+		$this->logger->expects($this->once())
+			->method('debug')
+			->with('IONOS mail config not available - no user session');
+
+		$result = $this->service->isMailConfigAvailable();
+
+		$this->assertFalse($result);
+	}
+
+	public function testIsMailConfigAvailableReturnsTrueWhenUserHasNoRemoteAccount(): void {
+		$user = $this->createMock(IUser::class);
+		$user->method('getUID')->willReturn('testuser');
+
+		$this->ionosConfigService->expects($this->once())
+			->method('isIonosIntegrationEnabled')
+			->willReturn(true);
+
+		$this->userSession->expects($this->once())
+			->method('getUser')
+			->willReturn($user);
 
 		$this->ionosMailService->expects($this->once())
 			->method('mailAccountExistsForCurrentUser')
 			->willReturn(false);
 
-		$this->logger->expects($this->never())
-			->method('debug');
+		$this->accountService->expects($this->never())
+			->method('findByUserIdAndAddress');
 
 		$result = $this->service->isMailConfigAvailable();
 
 		$this->assertTrue($result);
 	}
 
-	public function testIsMailConfigAvailableReturnsFalseWhenUserHasAccount(): void {
+	public function testIsMailConfigAvailableReturnsFalseWhenUserHasRemoteAndLocalAccount(): void {
+		$user = $this->createMock(IUser::class);
+		$user->method('getUID')->willReturn('testuser');
+
 		$this->ionosConfigService->expects($this->once())
 			->method('isIonosIntegrationEnabled')
 			->willReturn(true);
+
+		$this->userSession->expects($this->once())
+			->method('getUser')
+			->willReturn($user);
 
 		$this->ionosMailService->expects($this->once())
 			->method('mailAccountExistsForCurrentUser')
 			->willReturn(true);
 
+		$this->ionosMailService->expects($this->once())
+			->method('getIonosEmailForUser')
+			->with('testuser')
+			->willReturn('testuser@ionos.com');
+
+		// Return a non-empty array to simulate that a local account exists
+		$mockAccount = $this->createMock(\OCA\Mail\Account::class);
+		$this->accountService->expects($this->once())
+			->method('findByUserIdAndAddress')
+			->with('testuser', 'testuser@ionos.com')
+			->willReturn([$mockAccount]);
+
 		$this->logger->expects($this->once())
 			->method('debug')
-			->with('IONOS mail config not available - user already has an account');
+			->with('IONOS mail config not available - user already has account configured locally', [
+				'email' => 'testuser@ionos.com',
+			]);
+
+		$result = $this->service->isMailConfigAvailable();
+
+		$this->assertFalse($result);
+	}
+
+	public function testIsMailConfigAvailableReturnsTrueWhenUserHasRemoteAccountButNotLocal(): void {
+		$user = $this->createMock(IUser::class);
+		$user->method('getUID')->willReturn('testuser');
+
+		$this->ionosConfigService->expects($this->once())
+			->method('isIonosIntegrationEnabled')
+			->willReturn(true);
+
+		$this->userSession->expects($this->once())
+			->method('getUser')
+			->willReturn($user);
+
+		$this->ionosMailService->expects($this->once())
+			->method('mailAccountExistsForCurrentUser')
+			->willReturn(true);
+
+		$this->ionosMailService->expects($this->once())
+			->method('getIonosEmailForUser')
+			->with('testuser')
+			->willReturn('testuser@ionos.com');
+
+		$this->accountService->expects($this->once())
+			->method('findByUserIdAndAddress')
+			->with('testuser', 'testuser@ionos.com')
+			->willReturn([]);
+
+		$this->logger->expects($this->once())
+			->method('debug')
+			->with('IONOS mail config available - remote account exists but not configured locally', [
+				'email' => 'testuser@ionos.com',
+			]);
+
+		$result = $this->service->isMailConfigAvailable();
+
+		$this->assertTrue($result);
+	}
+
+	public function testIsMailConfigAvailableReturnsFalseWhenEmailCannotBeRetrieved(): void {
+		$user = $this->createMock(IUser::class);
+		$user->method('getUID')->willReturn('testuser');
+
+		$this->ionosConfigService->expects($this->once())
+			->method('isIonosIntegrationEnabled')
+			->willReturn(true);
+
+		$this->userSession->expects($this->once())
+			->method('getUser')
+			->willReturn($user);
+
+		$this->ionosMailService->expects($this->once())
+			->method('mailAccountExistsForCurrentUser')
+			->willReturn(true);
+
+		$this->ionosMailService->expects($this->once())
+			->method('getIonosEmailForUser')
+			->with('testuser')
+			->willReturn(null);
+
+		$this->logger->expects($this->once())
+			->method('warning')
+			->with('IONOS remote account exists but email could not be retrieved');
+
+		$this->accountService->expects($this->never())
+			->method('findByUserIdAndAddress');
 
 		$result = $this->service->isMailConfigAvailable();
 
@@ -91,8 +216,8 @@ class IonosMailConfigServiceTest extends TestCase {
 
 		$exception = new \Exception('Test exception');
 
-		$this->ionosMailService->expects($this->once())
-			->method('mailAccountExistsForCurrentUser')
+		$this->userSession->expects($this->once())
+			->method('getUser')
 			->willThrowException($exception);
 
 		$this->logger->expects($this->once())
