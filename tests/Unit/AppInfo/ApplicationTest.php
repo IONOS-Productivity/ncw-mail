5892da03d9cdd4f952b8cd5b47da09a943bc48ee
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-12-23 16:55:50 +0100

    IONOS(ionos-mail): feat(provider): register IonosProvider during application bootstrap
    Register IonosProvider in Application.boot() with exception handling and logging.
    Add unit tests for constructor, provider registration flow, and error handling.
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- tests/Unit/AppInfo/ApplicationTest.php
+++ tests/Unit/AppInfo/ApplicationTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2016-2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2015-2016 ownCloud, Inc.
@@ -7,14 +9,83 @@
  */
 namespace OCA\Mail\Tests\Unit\AppInfo;
 
-use ChristophWurst\Nextcloud\Testing\TestCase;
 use OCA\Mail\AppInfo\Application;
+use OCA\Mail\Provider\MailAccountProvider\Implementations\IonosProvider;
+use OCA\Mail\Provider\MailAccountProvider\ProviderRegistryService;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\IServerContainer;
+use Psr\Log\LoggerInterface;
+use Test\TestCase;
 
 class ApplicationTest extends TestCase {
-	public function testConstrucor() {
+	private Application $application;
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->application = new Application();
+	}
+
+	public function testConstructor(): void {
 		// Not really a test – it's just about code coverage
 		new Application();
 
 		$this->addToAssertionCount(1);
+	}
+
+	public function testBootRegistersIonosProvider(): void {
+		$bootContext = $this->createMock(IBootContext::class);
+		$serverContainer = $this->createMock(IServerContainer::class);
+		$providerRegistry = $this->createMock(ProviderRegistryService::class);
+		$ionosProvider = $this->createMock(IonosProvider::class);
+
+		$bootContext->expects($this->once())
+			->method('getServerContainer')
+			->willReturn($serverContainer);
+
+		$serverContainer->expects($this->exactly(2))
+			->method('get')
+			->willReturnMap([
+				[ProviderRegistryService::class, $providerRegistry],
+				[IonosProvider::class, $ionosProvider],
+			]);
+
+		$providerRegistry->expects($this->once())
+			->method('registerProvider')
+			->with($ionosProvider);
+
+		$this->application->boot($bootContext);
+	}
+
+	public function testBootHandlesExceptionGracefully(): void {
+		$bootContext = $this->createMock(IBootContext::class);
+		$serverContainer = $this->createMock(IServerContainer::class);
+		$logger = $this->createMock(LoggerInterface::class);
+
+		$bootContext->expects($this->once())
+			->method('getServerContainer')
+			->willReturn($serverContainer);
+
+		$exception = new \Exception('Provider registration failed');
+
+		$serverContainer->expects($this->exactly(2))
+			->method('get')
+			->willReturnCallback(function ($class) use ($exception, $logger) {
+				if ($class === ProviderRegistryService::class) {
+					throw $exception;
+				}
+				if ($class === LoggerInterface::class) {
+					return $logger;
+				}
+				throw new \Exception('Unexpected class: ' . $class);
+			});
+
+		$logger->expects($this->once())
+			->method('error')
+			->with('Failed to register mail account providers', [
+				'exception' => $exception,
+			]);
+
+		// Should not throw - exception should be caught and logged
+		$this->application->boot($bootContext);
 	}
 }
