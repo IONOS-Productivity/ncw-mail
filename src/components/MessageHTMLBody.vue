bf84fdb40f8ad94876c2177b05e4f38c53ca5bdf
Arsalan Ul Haq Sohni <arsalan-ul-haq.sohni@strato.de>
2026-02-03 14:00:43 +0100

    IONOS(mail): remove !important for the images blocked text to be visible
    Signed-off-by: Arsalan Ul Haq Sohni <arsalan-ul-haq.sohni@strato.de>

--- src/components/MessageHTMLBody.vue
+++ src/components/MessageHTMLBody.vue
@@ -179,7 +179,7 @@ export default {
 
 #mail-message-has-blocked-content {
 	margin-inline-start: 10px;
-	color: var(--color-text-maxcontrast) !important;
+	color: var(--color-text-maxcontrast);
 }
 
 #message-container {
