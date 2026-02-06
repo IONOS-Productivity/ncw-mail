b67c8cf9cb95bb23fb4c97d6b3017763cd900ce2
Misha M.-Kupriyanov <kupriyanov@strato.de>
2026-02-06 10:46:07 +0000

    IONOS(ionos-mail): feat: Add frontend components for mailbox administration
    - Create MailboxAdminService for API integration
    - Create MailboxAdministration main component with list view
    - Create MailboxListItem for individual mailbox rows with delete action
    - Create MailboxDeletionModal for delete confirmation
    - Implement loading, error, and empty states
    - Add user avatar and details display
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

---
+++ src/components/provider/mailbox/ProviderMailboxListItem.vue
@@ -0,0 +1,106 @@
+<!--
+  - SPDX-FileCopyrightText: 2026 STRATO GmbH
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<template>
+	<tr class="mailbox-list-item">
+		<td class="email-column">
+			<span class="email-address">{{ mailbox.email }}</span>
+		</td>
+		<td class="user-column">
+			<div class="user-info">
+				<NcAvatar :user="mailbox.userId"
+					:size="32"
+					:display-name="mailbox.name || mailbox.userId" />
+				<div class="user-details">
+					<span class="user-name">{{ mailbox.name || mailbox.userId }}</span>
+					<span class="user-id">{{ mailbox.userId }}</span>
+				</div>
+			</div>
+		</td>
+		<td class="actions-column">
+			<div class="actions">
+				<NcActions>
+					<NcActionButton @click="$emit('delete', mailbox)">
+						<template #icon>
+							<IconDelete :size="20" />
+						</template>
+						{{ t('mail', 'Delete') }}
+					</NcActionButton>
+				</NcActions>
+			</div>
+		</td>
+	</tr>
+</template>
+
+<script>
+import { NcAvatar, NcActions, NcActionButton } from '@nextcloud/vue'
+import IconDelete from 'vue-material-design-icons/Delete.vue'
+
+export default {
+	name: 'ProviderMailboxListItem',
+	components: {
+		NcAvatar,
+		NcActions,
+		NcActionButton,
+		IconDelete,
+	},
+	props: {
+		mailbox: {
+			type: Object,
+			required: true,
+		},
+	},
+	emits: ['delete'],
+}
+</script>
+
+<style scoped lang="scss">
+.mailbox-list-item {
+	td {
+		padding: 12px;
+		vertical-align: middle;
+	}
+
+	.email-column {
+		.email-address {
+			font-family: monospace;
+			font-size: 14px;
+		}
+	}
+
+	.user-column {
+		.user-info {
+			display: flex;
+			align-items: center;
+			gap: 12px;
+
+			.user-details {
+				display: flex;
+				flex-direction: column;
+				gap: 2px;
+
+				.user-name {
+					font-weight: 500;
+					font-size: 14px;
+				}
+
+				.user-id {
+					font-size: 12px;
+					color: var(--color-text-lighter);
+				}
+			}
+		}
+	}
+
+	.actions-column {
+		text-align: end;
+
+		.actions {
+			display: flex;
+			justify-content: flex-end;
+		}
+	}
+}
+</style>
